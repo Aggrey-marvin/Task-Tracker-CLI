0b5bb3ea5ded10d5917f0312f094d756257a69c2
Aggrey-marvin <zijjaaggreymarvin@gmail.com>
2025-04-22 14:14:25 +0300

    Begining

---
+++ App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class App {
+    public static void main(String [] args) {
+        System.out.println("===============Task CLI===============");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        
+    }
+}
