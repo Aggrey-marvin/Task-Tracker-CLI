d8dcb0900edb61da59b23bb6689d6ad392db8232
Aggrey-marvin <zijjaaggreymarvin@gmail.com>
2025-04-23 14:11:40 +0300

    Start the logic

--- App.java
+++ App.java
@@ -1,9 +1,29 @@
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class App {
-    public static void main(String [] args) {
-        System.out.println("===============Task CLI===============");
+    private static ArrayList<String> expectedArgs = new ArrayList<String>(Arrays.asList("new-list", "task-list", "delete-task"));
 
-
-        
+    private static void printOptions() {
+        System.out.println("Here are the available functions");
+        System.out.println();
+        System.out.println("-> task-cli new-list <name> - Create a new task list");
+        System.out.println("-> task-cli task-list - List your task lists");
+        System.out.println("-> task-cli delete-list <id> - List your task lists");
+        System.out.println();
+        System.out.println("====================Task CLI \u00A9 Pearl Tech " + Year.now().getValue() + " ============");
     }
+
+    public static void main(String [] args) {
+        // Validations
+        if (args.length == 0) {
+            System.out.println("====================Welcome to Task CLI====================");
+            printOptions();
+        } else if (!expectedArgs.contains(args[0])) {
+            System.out.println("====================Task CLI====================");
+            System.out.println("Invalid option :(");
+            printOptions();
+        }
+    } 
 }
--- TaskList.java
+++ TaskList.java
@@ -2,4 +2,20 @@ import java.util.ArrayList;
 
 public class TaskList {
     private ArrayList<Task> [] tasks;
+    private int userId;
+    private String userName;
+    private String alias;
+
+    public ArrayList<Task>[] getTasks() {
+        return tasks;
+    }
+    public int getUserId() {
+        return userId;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String getAlias() {
+        return alias;
+    }
 }
